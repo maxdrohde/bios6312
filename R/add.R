7ac0a45901ace586a3733b0f3b06c1b98939ce3f
Max Rohde <maxdrohde@gmail.com>
2021-03-02 14:13:50 -0600

    push

---
+++ R/add.R
@@ -0,0 +1,33 @@
+#' Adds two numbers
+#'
+#' Adds two number together using R
+#'
+#'
+#' @param a numeric
+#' @param b numeric
+#'
+#' @return numeric
+#' @export
+#' @examples
+#' add(5,8)
+add <- function(a,b){
+  return(a+b)
+}
+
+##################################################
+
+#' Subtracts two numbers
+#'
+#' Subtracts two number together using R
+#'
+#'
+#' @param a numeric
+#' @param b numeric
+#'
+#' @return numeric
+#' @export
+#' @examples
+#' subtract(5,8)
+subtract <- function(a,b){
+  return(a-b)
+}
